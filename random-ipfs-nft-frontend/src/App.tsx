7a4133258fef963fa31ec66a8ce3c958280b2e71
Nawodya Ishan <nawodyain@gmail.com>
2024-02-12 01:49:06 +0530

    feat: add vite project with shadcn

---
+++ random-ipfs-nft-frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import {Button} from "@/components/ui/button.tsx";
+
+function App() {
+
+
+    return (
+        <>
+            <Button>Click me</Button>
+        </>
+    )
+}
+
+export default App
