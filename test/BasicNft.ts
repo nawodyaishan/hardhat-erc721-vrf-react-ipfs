b1b05b375c2a0c2a7e56996bf8e08fb150411994
Nawodya Ishan <nawodyain@gmail.com>
2024-02-11 19:01:56 +0530

    feat: implement basic tests

--- test/BasicNft.ts
+++ test/BasicNft.ts
@@ -1,61 +1,61 @@
-import { ethers } from "hardhat";
-import { expect } from "chai";
-import { BasicNft } from "../typechain-types";
-import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { ethers } from 'hardhat';
+import { expect } from 'chai';
+import { BasicNft } from '../typechain-types';
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
 
-describe("BasicNft", function () {
+describe('BasicNft', function () {
   let basicNft: BasicNft;
   let owner: SignerWithAddress, addr1: SignerWithAddress;
 
   beforeEach(async function () {
     [owner, addr1] = await ethers.getSigners();
 
-    const BasicNftFactory = await ethers.getContractFactory("BasicNft", owner);
+    const BasicNftFactory = await ethers.getContractFactory('BasicNft', owner);
     basicNft = await BasicNftFactory.deploy();
   });
 
-  describe("Deployment", function () {
-    it("Should correctly set the initial token counter to 0 ✅", async function () {
+  describe('Deployment', function () {
+    it('Should correctly set the initial token counter to 0 ✅', async function () {
       expect(await basicNft.getTokenCounter()).to.equal(0);
     });
 
-    it("Should have correct name and symbol ✅", async function () {
-      expect(await basicNft.name()).to.equal("Dogie");
-      expect(await basicNft.symbol()).to.equal("DOG");
+    it('Should have correct name and symbol ✅', async function () {
+      expect(await basicNft.name()).to.equal('Dogie');
+      expect(await basicNft.symbol()).to.equal('DOG');
     });
   });
 
-  describe("Minting", function () {
-    it("Should allow owner to mint a new token ✅", async function () {
+  describe('Minting', function () {
+    it('Should allow owner to mint a new token ✅', async function () {
       await expect(basicNft.mintNft())
-        .to.emit(basicNft, "Transfer")
+        .to.emit(basicNft, 'Transfer')
         .withArgs(ethers.ZeroAddress, owner.address, 0);
       expect(await basicNft.ownerOf(0)).to.equal(owner.address);
     });
 
-    it("Should increment token counter after minting ✅", async function () {
+    it('Should increment token counter after minting ✅', async function () {
       await basicNft.mintNft();
       expect(await basicNft.getTokenCounter()).to.equal(1);
     });
   });
 
-  describe("Token URI", function () {
-    it("Should return the correct token URI for minted tokens ✅", async function () {
+  describe('Token URI', function () {
+    it('Should return the correct token URI for minted tokens ✅', async function () {
       await basicNft.mintNft();
       expect(await basicNft.tokenURI(0)).to.equal(
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json",
+        'ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json',
       );
     });
 
-    it("Should fail when querying URI for a non-existent token 🟥", async function () {
+    it('Should fail when querying URI for a non-existent token 🟥', async function () {
       await expect(basicNft.tokenURI(0))
-        .to.be.revertedWithCustomError(basicNft, "ERC721NonexistentToken")
+        .to.be.revertedWithCustomError(basicNft, 'ERC721NonexistentToken')
         .withArgs(0);
     });
   });
 
-  describe("Token Counter", function () {
-    it("Should return the correct number of tokens minted ✅", async function () {
+  describe('Token Counter', function () {
+    it('Should return the correct number of tokens minted ✅', async function () {
       await basicNft.mintNft();
       expect(await basicNft.getTokenCounter()).to.equal(1);
     });
