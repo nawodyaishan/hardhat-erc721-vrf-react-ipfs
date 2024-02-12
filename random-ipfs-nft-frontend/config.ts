5fd35a34c61420d3d1348dd192bbac1969b7a70e
Nawodya Ishan <nawodyain@gmail.com>
2024-02-12 12:41:28 +0530

    feat: add prettier

--- random-ipfs-nft-frontend/config.ts
+++ random-ipfs-nft-frontend/config.ts
@@ -1,17 +1,13 @@
-import { http, createConfig } from 'wagmi'
-import { sepolia } from 'wagmi/chains'
-import { injected, safe, walletConnect } from 'wagmi/connectors'
+import { http, createConfig } from 'wagmi';
+import { sepolia } from 'wagmi/chains';
+import { injected, safe, walletConnect } from 'wagmi/connectors';
 
-const projectId = '792d3d84424f76d7cab738b48f329d81'
+const projectId = '792d3d84424f76d7cab738b48f329d81';
 
 export const config = createConfig({
-    chains: [sepolia],
-    connectors: [
-        injected(),
-        walletConnect({ projectId }),
-        safe(),
-    ],
-    transports: {
-        [sepolia.id]: http(),
-    },
-})
+  chains: [sepolia],
+  connectors: [injected(), walletConnect({ projectId }), safe()],
+  transports: {
+    [sepolia.id]: http(),
+  },
+});
--- random-ipfs-nft-frontend/src/App.tsx
+++ random-ipfs-nft-frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import './App.css'
-import {Button} from "@/components/ui/button.tsx";
-import {WagmiProvider} from "wagmi";
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
-import {config} from "../config.ts";
-import {ConnectKitButton, ConnectKitProvider} from "connectkit";
+import './App.css';
+import { Button } from '@/components/ui/button.tsx';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { config } from '../config.ts';
+import { ConnectKitButton, ConnectKitProvider } from 'connectkit';
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
-    return (
-        <WagmiProvider config={config}>
-            <QueryClientProvider client={queryClient}>
-                <ConnectKitProvider>
-                    <Button>Click me</Button>
-                    <ConnectKitButton/>
-                </ConnectKitProvider>
-            </QueryClientProvider>
-        </WagmiProvider>
-    )
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>
+          <Button>Click me</Button>
+          <ConnectKitButton />
+        </ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
 
-export default App
+export default App;
